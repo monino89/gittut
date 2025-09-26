fd100d089b9b0e7f85370289aec790e4d7f25300
CENTIC <CENTIC@10.6.65.247>
2025-09-26 10:32:37 -0500

    ola3

--- src/gitcosa/Gitcosa.java
+++ src/gitcosa/Gitcosa.java
@@ -1,18 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gitcosa;
-
-/**
- *
- * @author CENTIC
- */
 public class Gitcosa {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("hola");
         System.out.println("ojadskjfl");
